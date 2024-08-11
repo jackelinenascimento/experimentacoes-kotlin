6c179bda7a52652f7a2a24d3f2143992205f7d0a
Jackeline Nascimento <jackeline_nascimento@hotmail.com>
2024-08-10 21:45:44 -0300

    primeiras configuracoes

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.1.2"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.spring") version "1.9.10"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<BootJar>{
+    mainClass.set("org.example.Application")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "experimentacoes-kotlin"
+
