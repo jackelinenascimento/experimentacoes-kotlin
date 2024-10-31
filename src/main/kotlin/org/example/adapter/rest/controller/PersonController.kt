955a3cdfc73bcb7ae6920d48d6d819b8eeae21e2
Jackeline Nascimento <jackeline_nascimento@hotmail.com>
2024-10-31 12:06:07 -0300

    estrutura de logs

--- src/main/kotlin/org/example/adapter/rest/controller/PersonController.kt
+++ src/main/kotlin/org/example/adapter/rest/controller/PersonController.kt
@@ -6,6 +6,8 @@ import org.example.adapter.rest.dto.PersonResponseDTO
 import org.example.domain.model.Person
 import org.example.domain.ports.input.PersonInputPort
 import org.example.exception.EntityNotFoundException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -21,11 +23,15 @@ import java.util.UUID
 @RequestMapping("/persons")
 class PersonController(private val personInputPort: PersonInputPort) {
 
+    private val logger: Logger = LoggerFactory.getLogger(PersonController::class.java)
+
     @PostMapping
     fun savePerson(
         @Valid @RequestBody
         personRequestDTO: PersonRequestDTO
     ): ResponseEntity<PersonResponseDTO> {
+        logger.info("action=PersonController.savePerson - request: $personRequestDTO" )
+
         val person = personRequestDTO.toDomain()
         val savedPerson = personInputPort.savePerson(person)
         return ResponseEntity(PersonResponseDTO.fromDomain(savedPerson), HttpStatus.CREATED)
@@ -33,6 +39,8 @@ class PersonController(private val personInputPort: PersonInputPort) {
 
     @GetMapping
     fun getAllPersons(): ResponseEntity<List<Person>> {
+        logger.info("action=PersonController.getAllPersons")
+
         val persons = personInputPort.getAllPersons()
         if (persons.isEmpty()) {
             throw EntityNotFoundException("No persons found")
@@ -42,12 +50,16 @@ class PersonController(private val personInputPort: PersonInputPort) {
 
     @GetMapping("/{id}")
     fun getPersonById(@PathVariable id: UUID): ResponseEntity<Person> {
+        logger.info("action=PersonController.getPersonById - id: $id")
+
         val person = personInputPort.getPersonById(id) ?: throw EntityNotFoundException("Person with id $id not found")
         return ResponseEntity(person, HttpStatus.OK)
     }
 
     @DeleteMapping
     fun deletePersonById(@PathVariable id: UUID): ResponseEntity<Unit> {
+        logger.info("action=PersonController.deletePersonById - id: $id")
+
         personInputPort.deletePerson(id)
         return ResponseEntity(HttpStatus.NO_CONTENT)
     }
